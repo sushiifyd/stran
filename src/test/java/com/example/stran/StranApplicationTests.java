6b054941d031d1eb859e08e09a33eaef563c1f06
Shushan <shushan.nazaretyan@hilton.com>
2026-02-05 14:41:04 -0800

    Initialize Spring Boot project structure

---
+++ src/test/java/com/example/stran/StranApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.stran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StranApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
